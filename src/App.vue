edc45b500fa886bb72531f1d211692e7c431f4c5
Vlad Crauciuc <vlad.crauciuc1@gmail.com>
2022-08-11 11:46:32 +0300

    *

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="absolute top-0 left-0">
-    <div class="mx-auto flex flex-col justify-center w-screen h-screen">
-      <nav
-        class="bg-[#191919] flex items-center text-white min-h-[64px] sm:min-h-[80px]"
+  <div class="mx-auto flex flex-col justify-center w-screen h-screen">
+    <nav
+      class="bg-[#191919] flex items-center text-white min-h-[64px] sm:min-h-[80px]"
+    >
+      <div class="container mx-auto flex justify-between items-center">
+        <Nav />
+      </div>
+    </nav>
+    <main
+      class="container mx-auto flex justify-center items-start h-full py-16*"
+    >
+      <router-view />
+    </main>
+    <footer class="bg-red-500* flex flex-col items-center">
+      <span class="py-3">Copyright &copy; Vlad Crauciuc, 2022</span>
+      <router-link
+        to="/contact"
+        class="bg-orange-400 py-4 w-full text-center text-white"
+        >Want to work together? Contact me</router-link
       >
-        <div class="container mx-auto flex justify-between items-center">
-          <Nav />
-        </div>
-      </nav>
-      <main
-        class="container mx-auto flex justify-center items-start h-full py-16*"
-      >
-        <router-view />
-      </main>
-      <footer class="bg-red-500* flex flex-col items-center">
-        <span class="py-3">Copyright &copy; Vlad Crauciuc, 2022</span>
-        <router-link
-          to="/contact"
-          class="bg-orange-400 py-4 w-full text-center text-white"
-          >Want to work together? Contact me</router-link
-        >
-      </footer>
-    </div>
+    </footer>
   </div>
 </template>
 
